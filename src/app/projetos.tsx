db17ea19a164d3facd3bcd2990589037d76e7064
Kauã Klein <kaua1floripa@gmail.com>
2025-02-13 15:08:41 -0300

    portfolio 1.0

---
+++ src/app/projetos.tsx
@@ -0,0 +1,10 @@
+// src/app/projetos/page.tsx
+export default function Projetos() {
+    return (
+      <div>
+        <h1>Meus Projetos</h1>
+        {/* Adicione o conteúdo da sua página de projetos aqui */}
+      </div>
+    );
+  }
+  
